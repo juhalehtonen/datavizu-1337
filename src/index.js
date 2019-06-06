528192c871bb5530efe5d1686d0cfd40cc6169be
Juha Lehtonen <hello@juha.tl>
2019-06-06 20:05:25 +0300

    Provide non-demo visualization code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const dscc = require('@google/dscc');
-const viz = require('@google/dscc-scripts/viz/initialViz.js');
+// const viz = require('@google/dscc-scripts/viz/initialViz.js');
 const local = require('./localMessage.js');
 
 // change this to 'true' for local development
@@ -7,13 +7,42 @@ const local = require('./localMessage.js');
 export const LOCAL = false;
 
 // write viz code here
-const drawViz = (data) => {
-  viz.firstViz(data);
-};
+// const drawViz = (data) => {
+//   viz.firstViz(data);
+// };
 
-// renders locally
+const drawViz = (data) => {
+
+    // set margins + canvas size
+    const margin = { top: 10, bottom: 50, right: 10, left: 10 };
+    const height = dscc.getHeight() - margin.top - margin.bottom;
+    const width = dscc.getWidth() - margin.left - margin.right;
+
+    // remove the svg if it already exists
+    if (document.querySelector("svg")) {
+        let oldSvg = document.querySelector("svg");
+        oldSvg.parentNode.removeChild(oldSvg);
+    }
+
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute("height", `${height}px`);
+    svg.setAttribute("width", `${width}px`);
+
+    const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    rect.setAttribute('width', `${width/2}px`);
+    rect.setAttribute('height', `${height/2}px`);
+    rect.style.fill =  'blue';
+
+    svg.append(rect);
+
+    document.body.appendChild(svg);
+}
+
+
 if (LOCAL) {
-  drawViz(local.message);
+    // renders locally
+    drawViz(local.message);
 } else {
-  dscc.subscribeToData(drawViz, {transform: dscc.objectTransform});
+    // subscribe to data and style changes
+    dscc.subscribeToData(drawViz, {transform: dscc.objectTransform});
 }
